f0dabcd8e733cd1bd91b091b8881b731632be302
Ingvar <igor.stepanov2000@gmail.com>
2026-04-30 17:33:44 +0300

    pump version

--- gliclass/__init__.py
+++ gliclass/__init__.py
@@ -1,3 +1,5 @@
 from .model import GLiClassModel, GLiClassBiEncoder, GLiClassUniEncoder, GLiClassEncoderDecoderCLS
 from .config import GLiClassModelConfig
 from .pipeline import ZeroShotClassificationPipeline, BiEncoderZeroShotClassificationPipeline, ZeroShotClassificationWithChunkingPipeline
+
+__version__ = "0.1.17"
